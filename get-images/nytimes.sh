d2c8bfdeb2212325d2d3e5f46cfd220222efe783
Colin McNaughton <cloin@users.noreply.github.com>
2024-03-18 15:50:36 -0700

    updates

--- get-images/nytimes.sh
+++ get-images/nytimes.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export TODAY=$(date +%Y/%m/%d)
-convert -density 300 "https://static01.nyt.com/images/$TODAY/nytfrontpage/scan.pdf" -trim +repage -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 "images/nytimes.jpeg"
+pdftoppm -f 1 -l 1 -r 300 "https://static01.nyt.com/images/$TODAY/nytfrontpage/scan.pdf" images/nytimes -png
+convert images/nytimes-1.png -trim +repage -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 images/nytimes.jpeg
