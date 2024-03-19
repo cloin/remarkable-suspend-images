edf55fa34636832aa1558bb5544ed8f6a075156e
Colin McNaughton <c.a.mcnaughton@gmail.com>
2024-03-19 17:23:18 -0400

    update

--- get-images/nytimes.sh
+++ get-images/nytimes.sh
@@ -16,7 +16,7 @@ curl -o images/nytimes.pdf "https://static01.nyt.com/images/$TODAY/nytfrontpage/
 pdftoppm -f 1 -l 1 -r 300 images/nytimes.pdf images/nytimes -png
 
 # Process the PNG image with ImageMagick to fit the reMarkable tablet's screen
-convert images/nytimes-1.png -trim +repage -chop 0x200 -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 png:test/nytimes_converted.png
+convert images/nytimes-1.png -trim +repage -chop 0x200 -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 png:images/nytimes_converted.png
 
 mv images/nytimes_converted-0.png images/nytimes.png
 # Clean up the downloaded PDF
