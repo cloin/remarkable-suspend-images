cfda73ae481cbebb0c67110cc2c937ed7ab4a5ba
colin <colin@redhat.com>
2024-03-20 10:32:46 -0400

    updates

--- image-scripts/nytimes.sh
+++ image-scripts/nytimes.sh
@@ -8,19 +8,25 @@ export SHORT_TODAY=$(date +%Y%m%d)
 mkdir -p images
 
 # Download the NYT front page PDF
+echo "getting image from:"
 echo "https://static01.nyt.com/images/$TODAY/nytfrontpage/scan.pdf"
-
 curl -o images/nytimes.pdf "https://static01.nyt.com/images/$TODAY/nytfrontpage/scan.pdf"
 
 
 # Convert the first page of the PDF to PNG using pdftoppm
+echo " convert pdf to png"
 pdftoppm -f 1 -l 1 -r 300 images/nytimes.pdf images/nytimes -png
 
 # Process the PNG image with ImageMagick to fit the reMarkable tablet's screen
-convert images/nytimes-1.png -trim +repage -chop 0x200 -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 png:images/nytimes_converted.png
+echo " crop to remarkable screen size"
+convert images/nytimes-1.png -trim +repage -chop 0x200 -resize "1364x1832^" -crop 1364x1832 -gravity center -extent 1404x1872 png:images/nytimes-converted.png
 
-mv images/nytimes_converted-0.png images/nytimes_$SHORT_TODAY.png
+# Rename files
+echo " rename image files"
+mv images/nytimes-converted-0.png images/nytimes_$SHORT_TODAY.png
 rm images/nytimes.png
 cp images/nytimes_$SHORT_TODAY.png images/nytimes.png
-# Clean up the downloaded PDF
+
+# Clean up unused files
 rm images/nytimes.pdf
+rm images/nytimes-*
